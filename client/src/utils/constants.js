b3e8823f99cfbc9a7cc3a8eec306f6c5a918a197
staticGuru <vigneshguru274@gmail.com>
2023-01-23 20:51:52 +0530

    add the blockchain part

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+export const contractAddress = "0x2a0BC0B2C4Fb026A57e436B5d1110174803e45A0"; // our contract deploy address
 export const contractABI = abi.abi;
